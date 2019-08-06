75d1fb533ba2f07a862a8d5c72c7f1750a0025d9
Brandyn Vay <brandynvay@protonmail.com>
2019-08-05 20:03:41 -0700

    done

--- file.js
+++ file.js
@@ -1,11 +1,23 @@
 'use strict';
 
 const uuid = require('uuid/v4');
+const fs = require('fs');
 
 class Model {
 
   constructor() {
     this.database = [];
+    fs.readFile('data/persons.db', (err,data) => {
+      let buff = data.toString();
+      if (err){ console.log(err); }
+      else {console.log(buff);}
+    });
+  }
+
+  saveToFs() {
+    fs.writeFile('data/persons.db', this.database, function (err) {
+      if (err) throw err;
+    });
   }
 
   get(id) {
@@ -19,6 +31,7 @@ class Model {
     if (record) {
       if (record.id) { this.database.push(record); }
     }
+    this.saveToFs();
     return Promise.resolve(record);
 
   }
@@ -26,11 +39,13 @@ class Model {
   update(id, entry) {
     let record = this.sanitize(entry);
     if (record.id) { this.database = this.database.map((item) => (item.id === id) ? record : item); }
+    this.saveToFs();
     return Promise.resolve(record);
   }
 
   delete(id) {
     this.database = this.database.filter((record) => record.id !== id);
+    this.saveToFs();
     return Promise.resolve();
   }
 
@@ -39,14 +54,18 @@ class Model {
     let record = entry;
     Object.keys(this.schema).forEach(field => {
       if (this.schema[field].required && !record[field]) {
+        console.log(`false on ${this.schema[field]}`)
         valid = false;
       }
       if (record[field] && this.schema[field].type && !(typeof record[field] === this.schema[field].type)) {
+        console.log(`false on ${this.schema[field]}`)
         valid = false;
       }
-    })
+    });
+    console.log(record, valid);
     return valid ? record : undefined;
   }
+
 }
 
 module.exports = Model;
